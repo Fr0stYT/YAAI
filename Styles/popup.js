9af6f2c08b5c9fe010eb9c1e8a47eeb54bc687d9
Frost_ <90537774+Fr0stYT@users.noreply.github.com>
2022-11-16 09:11:43 -0600

    Update popup.js

--- Styles/popup.js
+++ Styles/popup.js
@@ -1,1 +1,16 @@
+<html>
+<head>
+    <script type="text/javascript">
+        function open_win() {
+            //window.open("https://fr0styt.github.io/YAAI/")
+            window.location = "https://fr0styt.github.io/YAAI/"
+        }
+    </script>
+</head>
 
+<body>
+    <form>
+        <input type=6="navigate to" onclick="open_win()">
+    </form>
+</body>
+</html>
